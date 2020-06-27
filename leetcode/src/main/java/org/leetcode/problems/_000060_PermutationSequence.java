229a078a1eee34229b634d93da5ddddf8a8af1f3
AlexKokoz <alexandros.kokozidis@gmail.com>
2020-06-27 20:27:44 +0300

    MODIFIED: Problem 60

--- leetcode/src/main/java/org/leetcode/problems/_000060_PermutationSequence.java
+++ leetcode/src/main/java/org/leetcode/problems/_000060_PermutationSequence.java
@@ -8,36 +8,33 @@ package org.leetcode.problems;
  *
  */
 public class _000060_PermutationSequence {
+
 	public String getPermutation(int n, int k) {
-		k--;
-		int[] nums = new int[n];
-		for (int i = 0; i < n; i++)
-			nums[i] = i + 1;
-		while (k-- > 0) {
-			int next = n - 2;
-			while (next > 0 && nums[next] > nums[next + 1])
-				next--;
-			int rightMostGreatest = n - 1;
-			while (nums[rightMostGreatest] < nums[next])
-				rightMostGreatest--;
-			swap(next, rightMostGreatest, nums);
-			reverse(nums, next + 1, n - 1);
-		}
+		int[] pow = new int[n + 1];
+		pow[0] = 1;
+		for (int i = 1; i <= n; i++)
+			pow[i] = pow[i - 1] * i;
+		boolean[] used = new boolean[n + 1];
+
 		StringBuilder sb = new StringBuilder();
-		for (int num : nums)
-			sb.append(num);
+		for (int i = n; i >= 1; i--) {
+			int j = (int) Math.ceil((double) k / pow[i - 1]);
+			int elem = get(used, j);
+			used[elem] = true;
+			sb.append(elem);
+			k = k - (j - 1) * pow[i - 1];
+		}
 		return sb.toString();
 	}
 
-	void reverse(int[] arr, int lo, int hi) {
-		int mi = lo + ((hi - lo) >>> 1);
-		for (int i = 0; i + lo <= mi; i++)
-			swap(lo + i, hi - i, arr);
-	}
-
-	void swap(int i, int j, int[] arr) {
-		int swap = arr[i];
-		arr[i] = arr[j];
-		arr[j] = swap;
+	// get j-th not-used number in [1..n]
+	static int get(boolean[] used, int j) {
+		for (int i = 1; i < used.length; i++) {
+			if (!used[i])
+				j--;
+			if (j == 0)
+				return i;
+		}
+		return 0;
 	}
 }
